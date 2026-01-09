cfc771095143bedebe8ce27d8f6979f0290bfbf5
TeddyMuli <tmuli974@gmail.com>
2026-01-09 05:35:24 +0300

    implement modulo sharding algorithm

---
+++ shard_simulator/phase_1/pkg/algorithms/modulo.go
@@ -0,0 +1,26 @@
+package algorithms
+
+import "hash/crc32"
+
+type ModuloSharding struct {
+	nodes []string
+}
+
+func NewModulo() *ModuloSharding {
+	return &ModuloSharding{nodes: []string{}}
+}
+
+func (m *ModuloSharding) AddNode(nodeId string) {
+	m.nodes = append(m.nodes, nodeId)
+}
+
+func (m *ModuloSharding) GetShard(key string) string {
+	if len(m.nodes) == 0 {
+		return ""
+	}
+	
+	hash := crc32.ChecksumIEEE([]byte(key))
+	index := int(hash) % len(m.nodes)
+	
+	return m.nodes[index]
+}
