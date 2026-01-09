366951ec1b7a0b82c80e0c2ebb6a86bedee62f17
TeddyMuli <tmuli974@gmail.com>
2026-01-09 08:13:48 +0300

    add rendezvous algorithm

---
+++ shard_simulator/phase_1/pkg/algorithms/rendezvous.go
@@ -0,0 +1,41 @@
+package algorithms
+
+import (
+	"fmt"
+
+	"github.com/cespare/xxhash"
+	"golang.org/x/tools/go/callgraph/cha"
+)
+
+type RendezvousHashing struct {
+	nodes []string
+}
+
+func NewRendezvous() *RendezvousHashing {
+	return &RendezvousHashing{nodes: []string{}}
+}
+
+func (r *RendezvousHashing) Name() string {
+	return "Rendezvous Hashing"
+}
+
+func (r *RendezvousHashing) AddNode(nodeId string) {
+	r.nodes = append(r.nodes, nodeId)
+}
+
+func (r *RendezvousHashing) GetShard(key string) string {
+	var maxScore uint64
+	var champion string
+	
+	for _, node := range r.nodes {
+		hashInput := []byte(key + node)
+		score := xxhash.Sum64(hashInput)
+		
+		if score > maxScore {
+			maxScore = score
+			champion = node
+		}
+	}
+	
+	return champion
+}
