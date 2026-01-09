6cd3722159b5de8df618cebaa539b4d241dd6d52
TeddyMuli <tmuli974@gmail.com>
2026-01-09 08:50:37 +0300

    create csv utils

---
+++ shard_simulator/phase_1/pkg/utils/csv.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"os"
+	"fmt"
+	"encoding/csv"
+	"strconv"
+)
+
+func writeDistributionCSV(filename string, rows []DistRow) {
+    file, _ := os.Create(filename)
+    defer file.Close()
+    
+    writer := csv.NewWriter(file)
+    defer writer.Flush()
+
+    writer.Write([]string{"Algorithm", "ShardID", "KeyCount"}) // Header
+    for _, r := range rows {
+        writer.Write([]string{r.Algorithm, r.ShardID, strconv.Itoa(r.KeyCount)})
+    }
+}
+
+func writeMovementCSV(filename string, rows []MoveRow) {
+    file, _ := os.Create(filename)
+    defer file.Close()
+    
+    writer := csv.NewWriter(file)
+    defer writer.Flush()
+
+    writer.Write([]string{"Algorithm", "PercentMoved"}) // Header
+    for _, r := range rows {
+        writer.Write([]string{r.Algorithm, fmt.Sprintf("%.2f", r.PercentMoved)})
+    }
+}
