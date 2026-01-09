c5bfde5c2a414f41c04dd8385ee05c42684bb412
TeddyMuli <tmuli974@gmail.com>
2026-01-09 05:44:48 +0300

    implement statistics analyzer

---
+++ shard_simulator/phase_1/pkg/analyzer/stats.go
@@ -0,0 +1,26 @@
+package analyzer
+
+import "math"
+
+func CalculateSkew(distribution map[string]int) float64 {
+	var sum, count float64
+	values := []float64{}
+	
+	for _, v := range distribution {
+		val := float64(v)
+		sum += val
+		values = append(values, val)
+		count++
+	}
+	
+	mean := sum/count
+	
+	var varianceSum float64
+	for _, v := range values {
+		varianceSum += math.Pow(v-mean, 2)
+	}
+	
+	variance := varianceSum/count
+	
+	return math.Sqrt(variance)
+}
